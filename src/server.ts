288e51ddcf661f6f9d8155108694e670a6fff721
mdbayzid2025 <ssmdbayzid23@gmail.com>
2025-10-30 11:33:34 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ process.on('uncaughtException', error => {
 let server: any;
 async function main() {
   try {
-    mongoose.connect(config.database_url as string);
+  await  mongoose.connect(config.database_url as string);
     logger.info(colors.green('🚀 Database connected successfully'));
 
     //Seed Super Admin after database connection is successful
